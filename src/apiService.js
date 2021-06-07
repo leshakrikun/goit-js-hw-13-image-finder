f1f9c69091eff9bc6ff302219c549e787777328e
leshakrikun <lesha.krikun@users.noreply.github.com>
2021-06-07 21:41:41 +0300

    check

--- src/apiService.js
+++ src/apiService.js
@@ -1,7 +1,7 @@
 const myKey = '21938998-67fd96e5d4868b12a769f8729'
 
-const query = async (search) => {
-    const picture = await fetch(`https://pixabay.com/api/?key=${myKey}&image_type=photo&orientation=horizontal&q=${search}&page=1&per_page=12`);
+const query = async (search, counter) => {
+    const picture = await fetch(`https://pixabay.com/api/?key=${myKey}&image_type=photo&orientation=horizontal&q=${search}&page=${counter}&per_page=12`);
         return picture.json();
 };
 
--- src/index.js
+++ src/index.js
@@ -3,38 +3,64 @@ import './sass/main.scss';
 import query from './apiService.js'
 import cardTemplates from './cards.hbs'
 
+let markup; // разметка
+let counter = 0;
+let checkSearch = '';
 
-const refs = {
-    cardContainer: document.querySelector('.more')
-}
+
 const searchElem = document.querySelector('input');
 const loadMoreBtn = document.querySelector('.more');
-console.log(loadMoreBtn);
+
 function fetchQuery () {
     const search = searchElem.value.trim();
-    console.log(search);
-    const promise = query(search);
-    const q = promise.then(picture => draw(picture))
-}
+    counter += 1;
+    if(checkSearch !== search){
+        counter=1;
+    }
 
-let markup;
+    if(counter===1) {
+      checkSearch = search;
+      
+       if (document.querySelector('.gallery')) {
+            document.querySelector('.gallery').remove();
+          }
+    }
+    if (search.length===0){
+        return;
+    } else {
+    const promise = query(search, counter);
+    promise.then(picture => draw(picture));
+}}
+
 
 function draw(picture) {
+        
+    if (!document.querySelector('.gallery')) {
+        const ulElemNew = '<ul class="gallery"></ul>';
+        loadMoreBtn.insertAdjacentHTML('beforeBegin', ulElemNew);
+    } 
+
     markup = cardTemplates(picture.hits);
-    refs.cardContainer.insertAdjacentHTML('beforeBegin', markup);
-        if(picture.total>12) {
+    document.querySelector('.gallery').insertAdjacentHTML('beforeEnd', markup);
+        if(picture.total/counter>12) {
             loadMoreBtn.classList.remove('hidden')
+        } else {
+            loadMoreBtn.classList.add('hidden')
+            console.log('check');
         }
 } 
 
-//const search = document.getElementById('search-form').value;
+const showBtn = document.querySelector('.submit');
 
-const showBtn = document.querySelector('.submit')
-showBtn.addEventListener('click', fetchQuery)
+showBtn.addEventListener('click', fetchQuery);
 
-/* const element = document.getElementById('.more');
-element.scrollIntoView({
-  behavior: 'smooth',
-  block: 'end',
-});
- */
+loadMoreBtn.addEventListener('click', loadMore);
+
+function loadMore () {
+    loadMoreBtn.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end',
+        });
+    fetchQuery ();
+}
+
